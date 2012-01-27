a3251a999c905b2a5f9f1760187d56fc2ec774c6
Ariel <ariel.von.barnekow@upcnet.es>
2012-01-27 13:13:36 +0100

    Vista per generar les url per recollir les dades de l'usuari

--- upc/genwebCBL/browser/dadesUsuari.py
+++ upc/genwebCBL/browser/dadesUsuari.py
@@ -4,15 +4,20 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 
 
+class DadesUsuari():
+    params = ['servei', 'subservei', 'codiServei', 'codiSubservei']
+
+
 class DadesUsuariView(BrowserView):
 
     template = ViewPageTemplateFile('dadesUsuari.pt')
-    params = ['servei', 'subservei', 'codiServei', 'codiSubservei']
+    params = []
     dades = []
 
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        self.dades = DadesUsuari().params
         self.dades = self.request.form
 
     def __call__(self):
@@ -23,6 +28,10 @@ class DadesUsuariView(BrowserView):
                 "Falten dades, contacta amb el gestor del genweb", 'error')
                 self.request.response.redirect(self.get_redirect_url())
                 return
+        mt = getToolByName(self.context, 'portal_membership')
+        if mt.isAnonymousUser():
+            #  the user has not logged in
+            return
         return self.template()
 
     def _get_portal_url(self):
@@ -50,3 +59,41 @@ class DadesUsuariView(BrowserView):
 
     def get_codi_subservei(self):
         return self.dades['codiSubservei']
+
+
+class HelperView(BrowserView):
+
+    __call__ = ViewPageTemplateFile('dadesHelper.pt')
+    params = []
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.params = DadesUsuari().params
+        self.prettyNames = {'servei': 'Servei',
+                            'subservei': 'Subservei',
+                            'codiServei': 'Codi subservei',
+                            'codiSubservei': 'Codi Servei'
+                            }
+        self.dades = self.request.form
+
+    def has_data(self):
+        for a in self.params:
+            if a not in self.dades:
+                return False
+        return True
+
+    def get_params(self):
+        return self.params
+
+    def get_data(self):
+        return self.dades
+
+    def get_pretty(self, key):
+        return self.prettyNames[key]
+
+    def get_url(self):
+        ## Les linies comentades obtenen la url actual i fan un canvi...
+        # self.request["ACTUAL_URL"].replace('gn6-helper-url-recollir',
+        # 'gn6-recollir-dades')
+        return "gn6-recollir-dades?" + self.request["QUERY_STRING"]
